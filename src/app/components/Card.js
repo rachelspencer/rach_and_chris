6cfa8cd854da9bbfa9b6edd81fb11710beaa735e
Rachel Spencer <rachelspencer688@gmail.com>
2024-09-13 17:43:25 -0400

    install shadcn card component and add colors object to the tailwind config file to resolve an error msg

---
+++ src/app/components/Card.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+import {
+    Card as UICard,
+    // CardContent,
+    // CardDescription,
+    // CardFooter,
+    // CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+export default function Card() {
+    return (
+        <UICard>
+            <CardTitle>I am the card!</CardTitle>
+        </UICard>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
+import { Card } from '@/app/components'
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
-        Rach and Chris
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      R & C
+    </h1>
+      
       </main>
-      <footer className={styles.footer}>
-       
-      </footer>
+      <Card></Card>
     </div>
   );
 }
