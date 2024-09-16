9dc403568f7420892e4fc36b38281841e25959bb
Rachel Spencer <rachelspencer688@gmail.com>
2024-09-16 12:59:02 -0400

    un-install shadcn, re-install shadcn, specify location of global.css file during install, write catch all file paths for tailwind.config file

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,18 +1,77 @@
-import * as React from "react"
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+
 
 import {
     Card as UICard,
-    // CardContent,
-    // CardDescription,
-    // CardFooter,
-    // CardHeader,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
     CardTitle,
   } from "@/components/ui/card"
 
 export default function Card() {
+    const notifications = [
+        {
+          title: "Your call has been confirmed.",
+          description: "1 hour ago",
+        },
+        {
+          title: "You have a new message!",
+          description: "1 hour ago",
+        },
+        {
+          title: "Your subscription is expiring soon!",
+          description: "2 hours ago",
+        },
+      ]
+      
     return (
-        <UICard>
-            <CardTitle>I am the card!</CardTitle>
+        // <UICard className={cn("w-[380px]")}>
+        //     <CardTitle>I am the card!</CardTitle>
+        // </UICard>
+        <UICard className={cn("w-[380px]")}>
+            <CardHeader>
+                <CardTitle>Notifications</CardTitle>
+                <CardDescription>You have 3 unread messages.</CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+                <div className=" flex items-center space-x-4 rounded-md border p-4">
+                {/* <BellRing /> */}
+                <div className="flex-1 space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                    Push Notifications
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                    Send notifications to device.
+                    </p>
+                </div>
+                {/* <Switch /> */}
+                </div>
+                <div>
+                {notifications.map((notification, index) => (
+                    <div
+                    key={index}
+                    className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                    >
+                    <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                    <div className="space-y-1">
+                        <p className="text-sm font-medium leading-none">
+                        {notification.title}
+                        </p>
+                        <p className="text-sm text-muted-foreground">
+                        {notification.description}
+                        </p>
+                    </div>
+                    </div>
+                ))}
+                </div>
+            </CardContent>
+            <CardFooter>
+                Some footer text
+            </CardFooter>
         </UICard>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,61 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/app/page.js',
-    './src/app/layout.js',
-    './src/app/registry/page.js',
-    './src/app/rsvp/page.js',
-    './src/app/travel/page.js',
-    './src/app/wedding/page.js',
-    './src/components/Card.js',
+    darkMode: ['class'],
+    content: [
+    './src/app/**/*.{js,jsx}',
+    './src/components/**/*.{js,jsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--bg-background))",
-        border: "hsl(var(--border-border))",
-        foreground: "hsl(var(--text-foreground))",
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			border: 'hsl(var(--border))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	},
+  	plugins: []
   },
-  plugins: [],
-}
+    plugins: [require("tailwindcss-animate")]
 }
